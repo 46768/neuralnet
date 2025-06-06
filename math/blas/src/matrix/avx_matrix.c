66a14810058b6252bf56f972a281c4ee78e85e72
46768 <46768@bodin.ac.th>
2025-06-06 16:21:58 +0700

    feat: add empty avx matrix functions

--- math/blas/src/matrix/avx_matrix.c
+++ math/blas/src/matrix/avx_matrix.c
@@ -1,3 +1,23 @@
 #ifdef SIMD_AVX
+#include "matrix.h"
+
+// Matrix Operation
+
+void mat_vmul(Matrix* mat, Vector* vec) {
+}
+
+void mat_fmva(Matrix* mat1, Matrix* mat2, Vector* vec) {
+}
+
+void mat_hadamard(Matrix* mat, Vector* vec) {
+}
+
+void mat_t_hadamard(Matrix* mat, Vector* vec) {
+}
+
+// Vector Operation (Matrix return)
+
+void vec_crmul(Vector* c_vec, Vector* r_vec, Matrix* mat) {
+}
 
 #endif
