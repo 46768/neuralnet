b04e621b7f4ce4a467d8474c18881c0c3b40283a
46768 <46768@bodin.ac.th>
2025-06-06 15:56:54 +0700

    feat: add MatrixCtx struct for caching allocated size

--- math/blas/header/matrix.h
+++ math/blas/header/matrix.h
@@ -18,13 +18,33 @@ typedef struct {
 	float* data; /**< Data pointer of the matrix - float* (64bit)*/
 } Matrix;
 
+/**
+ * \struct MatrixCtx
+ * \brief A nxm-dimensional matrix context for optimized indexing
+ *
+ * 192 bit wide data
+ */
+typedef struct {
+	uint32_t sx; /**< Width of the matrix - uint32 (32bit)*/
+	uint32_t sy; /**< Height of the matrix - uint32 (32bit)*/
+	uint32_t rsx; /**< Allocated width of the matrix - uint32 (32bit)*/
+	uint32_t rsy; /**< Allocated height of the matrix - uint32 (32bit)*/
+	float* data; /**< Data pointer of the matrix - float* (64bit)*/
+} MatrixCtx;
+
 void mat_init(uint32_t, uint32_t, float*, Matrix*);
+
+#ifdef SIMD_AVX
+#	define get_mat_rsize(s) (s+7)&~7
+#else
+#	define get_mat_rsize(s) s
+#endif
 
 // Matrix indexing
 
-#define get_mat_ctx(ctx_name, mat) Matrix ctx_name = {mat->sx, mat->sy, mat->data}
-#define mat_idx(ctx, x, y) (ctx.data)[x+(y*(ctx.sx))]
-#define mat_t_idx(ctx, x, y) (ctx.data)[y+(x*(ctx.sy))]
+#define get_mat_ctx(ctx_name, mat) MatrixCtx ctx_name = {mat->sx, mat->sy, get_mat_rsize(mat->sx), get_mat_rsize(mat->sy), mat->data}
+#define mat_idx(ctx, x, y) (ctx.data)[x+(y*(ctx.rsx))]
+#define mat_t_idx(ctx, x, y) (ctx.data)[y+(x*(ctx.rsy))]
 
 // Matrix Operation
 
