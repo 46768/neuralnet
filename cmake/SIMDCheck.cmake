a28f92e2584cee4f294a1d405c389021a61a18e1
46768 <46768@bodin.ac.th>
2025-06-12 06:48:22 +0700

    feat: add avx512 check

--- cmake/SIMDCheck.cmake
+++ cmake/SIMDCheck.cmake
@@ -1,5 +1,15 @@
 include(CheckCSourceRuns)
 
+# Get AVX512 availbility
+check_c_source_runs("
+#include <cpuid.h>
+int main() {
+unsigned int eax,ebx,ecx,edx;
+__cpuid_count(7,0,eax,ebx,ecx,edx);
+return (ebx & (1 << 16)) == 0;
+}" haveAVX512
+)
+
 # Get AVX2 availability
 check_c_source_runs("
 #include <cpuid.h>
